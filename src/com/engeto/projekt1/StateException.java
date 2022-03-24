3c322e42bb0eaca2c783e6b87088bf730049eccd
FilipSery <98544649+FilipSery@users.noreply.github.com>
2022-03-24 20:33:47 +0100

    prace na tride StatesReadingFromFile

---
+++ src/com/engeto/projekt1/StateException.java
@@ -0,0 +1,8 @@
+package com.engeto.projekt1;
+
+public class StateException extends Exception{
+    public StateException(String message) {
+        super(message);
+
+    }
+}
--- src/com/engeto/projekt1/StatesReadingFromFile.java
+++ src/com/engeto/projekt1/StatesReadingFromFile.java
@@ -1,5 +1,36 @@
 package com.engeto.projekt1;
+import java.io.*;
+import javax.swing.plaf.nimbus.State;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class StatesReadingFromFile {
+    private final String File_Name = "vat-eu.csv";
+    private final String Delimiter = "\t";
+    ArrayList<StateInfo> stateList = new ArrayList<>();
 
+    public void loadPlantsFromFile (String fileName) throws StateException {
+        String delimiter = "\t";
+        try (Scanner s = new Scanner(new BufferedReader(new FileReader(fileName)))) {
+            while (s.hasNext()) {
+                String fromFile = s.nextLine();
+                String[] stateInformations = fromFile.split(Delimiter);
+                String stateAbbreviation;
+                String stateName;
+                int fullIncomeTax;
+                int loweredIncomeTax;
+                boolean hasSpecialTax;
+                try {
+                } catch (StateException e) {
+                    throw new StateException()
+                            ("Unavailable file (" + fileName + "), " + e.getLocalizedMessage());
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new StateException("Input file: "+fileName+" wasn't found"+e.getLocalizedMessage());
+        }
+    }
 }

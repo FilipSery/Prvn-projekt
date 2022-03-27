ccb1abc47ef7ff6d36580251020185d6db54db01
FilipSery <98544649+FilipSery@users.noreply.github.com>
2022-03-27 20:32:14 +0200

    vytvoreni ctece a nahravace do souboru ve tride StatesReadingFromFile

--- src/com/engeto/projekt1/StatesReadingFromFile.java
+++ src/com/engeto/projekt1/StatesReadingFromFile.java
@@ -12,25 +12,40 @@ public class StatesReadingFromFile {
     private final String Delimiter = "\t";
     ArrayList<StateInfo> stateList = new ArrayList<>();
 
-    public void loadPlantsFromFile (String fileName) throws StateException {
+    public void loadStatesFromFile(String fileName) throws StateException {
         String delimiter = "\t";
         try (Scanner s = new Scanner(new BufferedReader(new FileReader(fileName)))) {
             while (s.hasNext()) {
                 String fromFile = s.nextLine();
                 String[] stateInformations = fromFile.split(Delimiter);
-                String stateAbbreviation;
-                String stateName;
-                int fullIncomeTax;
-                int loweredIncomeTax;
-                boolean hasSpecialTax;
-                try {
-                } catch (StateException e) {
-                    throw new StateException()
-                            ("Unavailable file (" + fileName + "), " + e.getLocalizedMessage());
+                String stateAbbreviation = stateInformations[0];
+                String stateName = stateInformations[1];
+                int fullIncomeTax = Integer.parseInt(stateInformations[2]);
+                double loweredIncomeTax = Double.parseDouble(stateInformations[3]);
+                boolean hasSpecialTax = Boolean.parseBoolean(stateInformations[4]);
+
                 }
+            } catch(FileNotFoundException e){
+                throw new StateException("Input file: " + fileName + " wasn't found" + e.getLocalizedMessage());
+            } catch (NumberFormatException e) {
+                throw new StateException("Incorrect number format: "+e.getLocalizedMessage());
             }
-        } catch (FileNotFoundException e) {
-            throw new StateException("Input file: "+fileName+" wasn't found"+e.getLocalizedMessage());
+    }
+    public void saveFilesToFile(String fileName) throws StateException {
+        String delimiter = "\t";
+        String line = "\n";
+        //int numberOfLine = 0;
+        try (PrintWriter w = new PrintWriter(new BufferedWriter(new FileWriter(fileName)))) {
+            for (StateInfo s : stateList) {
+                String toFile = s.getStateAbbreviation();
+                toFile += delimiter + s.getStateName();
+                toFile += delimiter + s.getFullIncomeTax();
+                toFile += delimiter + s.getLoweredIncomeTax();
+                toFile += delimiter + s.isHasSpecialTax() + line;
+
+            }
+        } catch (IOException e) {
+            throw new StateException("Couldn't write to file: " + fileName + e.getLocalizedMessage());
         }
     }
 }
